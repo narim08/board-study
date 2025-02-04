bbe616b8e978aa7900e36c7049d165520dfc7d4a
Narim Park <skfla2459@gmail.com>
2025-02-04 14:13:44 +0900

    first commit

---
+++ src/main/java/studypj/boardpj/BoardpjApplication.java
@@ -0,0 +1,13 @@
+package studypj.boardpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardpjApplication.class, args);
+	}
+
+}
